5742b14498c71c687de71f1c03dc4b46f1896bd9
aarogyarijal <aarogya.rijal@gmail.com>
2020-06-15 01:55:50 +0545

    Added color options and option to clearas

--- scripts/main.js
+++ scripts/main.js
@@ -1,37 +1,78 @@
 const container = document.querySelector("#container");
-const refreshButton = document.querySelector("#refresh");
 const boxes = document.querySelectorAll("#container");
+const box = document.querySelector("#container").children;
+const clearButton = document.querySelector("#clear");
+const lengthButton = document.querySelector("#length");
+const whiteModeButton = document.querySelector("#white");
+const blackModeButton = document.querySelector("#black");
+const colorfulModeButton = document.querySelector("#colorful");
+let mode="white";
+let gridNumber=16;
 
 //Making Default Grid Divs
 for(let i=0; i<256; i++){
-    container.appendChild(document.createElement("div"));
+    let newChild=document.createElement("div");
+    newChild.setAttribute('class','box');
+    newChild.style.backgroundColor="rgba(255, 255, 255, 0.8)";
+    container.appendChild(newChild);
     container.style="grid-template-columns: repeat(16, auto)";
 }
 
-//When Refresh button is Clicked
-refreshButton.addEventListener("click", refreshButtonClicked)
-function refreshButtonClicked(){
+//Changing color of divs on mouse over
+blackModeButton.addEventListener("click", function() {mode="black"});
+whiteModeButton.addEventListener("click", function() {mode="white"});
+colorfulModeButton.addEventListener("click", function() {mode="colorful"});
+
+boxes.forEach(div => div.addEventListener("mouseover", changeColor));
+boxes.forEach(div => div.addEventListener("touchstart", changeColor)); //For mobile support, properly works on touching div
+function changeColor(e){
+    console.log(mode);
+    if(e.target.id === "container") return;
+    if (mode==="colorful"){
+        let r = Math.random()*255;
+        let g = Math.random()*255;
+        let b = Math.random()*255;
+        e.target.style.backgroundColor=`rgb(${r},${g},${b})`;
+    }
+    else if (mode==="white" || !mode){
+        e.target.style.backgroundColor="white";
+    }
+    else if (mode==="black"){
+        e.target.style.backgroundColor="black";
+    }
+}
+
+
+//When Length button is Clicked
+lengthButton.addEventListener("click", lengthButtonClicked);
+function lengthButtonClicked(){
     gridNumber = prompt("Grid Length?");
     container.innerHTML = "";
-for(let i=0; i<(gridNumber**2); i++){
-    let newChild=document.createElement("div");
-    container.style=`grid-template-columns: repeat(${gridNumber}, auto)`;
-    newChild.style.width=`calc(80vmin/${gridNumber}`;
-    newChild.style.height=`calc(80vmin/${gridNumber}`;
-    container.appendChild(newChild);
+    for(let i=0; i<(gridNumber**2); i++){
+        console.log("called");
+        let newChild=document.createElement("div");
+        newChild.setAttribute('class','box');
+        newChild.style.backgroundColor="rgba(255, 255, 255, 0.8)";
+        container.style=`grid-template-columns: repeat(${gridNumber}, auto)`;
+        newChild.style.width=`calc(80vmin/${gridNumber}`;
+        newChild.style.height=`calc(80vmin/${gridNumber}`;
+        container.appendChild(newChild);
+    }
 
 }
 
-}
-
-//Changing color of divs on mouse over
-boxes.forEach(div => div.addEventListener("mouseover", changeColor));
-boxes.forEach(div => div.addEventListener("touchstart", changeColor));
-function changeColor(e){
-    console.log(e);
-    if(e.target.id === "container") return;
-    let r = Math.random()*255;
-    let g = Math.random()*255;
-    let b = Math.random()*255;
-    e.target.style=`background-color:rgb(${r},${g},${b})`;
+//When Clear button is Clicked
+clearButton.addEventListener("click", clearButtonClicked);
+function clearButtonClicked(){
+    container.innerHTML = "";
+    for(let i=0; i<(gridNumber**2); i++){
+        console.log("called");
+        let newChild=document.createElement("div");
+        newChild.setAttribute('class','box');
+        newChild.style.backgroundColor="rgba(255, 255, 255, 0.8)";
+        container.style=`grid-template-columns: repeat(${gridNumber}, auto)`;
+        newChild.style.width=`calc(80vmin/${gridNumber}`;
+        newChild.style.height=`calc(80vmin/${gridNumber}`;
+        container.appendChild(newChild);
+    }
 }
